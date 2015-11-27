68c95eb214b98359354f209b76c5b49cedbc3837
Atticus White <contact@atticuswhite.com>
2015-11-27 13:38:59 -0500

    complete toggle component

---
+++ app/components/complete-toggle.js
@@ -0,0 +1,49 @@
+'use strict';
+
+import React, {
+  Component,
+  TouchableOpacity,
+  StyleSheet,
+  View,
+  Text
+} from 'react-native';
+
+class CompleteToggle extends Component {
+  toggle = () => {
+    if (this.props.checked) {
+      this.props.onUnchecked(this.props.index);
+    } else {
+      this.props.onChecked(this.props.index);
+    }
+  }
+  getStyle() {
+    if (this.props.checked) {
+      return styles.active;
+    } else {
+      return styles.inactive;
+    }
+  }
+  render() {
+    var todo = this.props.todo;
+    return (
+      <TouchableOpacity
+        style={[styles.button, this.getStyle()]}
+        onPress={this.toggle} />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  button: {
+    width: 20,
+    height: 20
+  },
+  active: {
+    backgroundColor: 'green'
+  },
+  inactive: {
+    backgroundColor: 'gray'
+  }
+});
+
+export default CompleteToggle;
--- app/components/todo-list.js
+++ app/components/todo-list.js
@@ -5,33 +5,21 @@ import React, {
   Component,
   View,
   Text,
-  TouchableOpacity,
   TextInput
 } from 'react-native';
+import CompleteToggle from './complete-toggle';
 
 class TodoList extends Component {
-  constructor(props) {
-    super(props);
-  }
-  toggleCompletion (todo, index) {
-    var {completeTodo, incompleteTodo} = this.props;
-    if (todo.completed) {
-      incompleteTodo(index);
-    } else {
-      completeTodo(index);
-    }
-  }
   render() {
-    var {addTodo} = this.props;
+    var {completeTodo, incompleteTodo} = this.props;
     var todos = this.props.todos.map((todo, index) => {
       return (
         <View key={index} style={styles.row}>
-          <TouchableOpacity
-            style={styles.text}
-            onPress={() => this.toggleCompletion(todo, index)}>
-            <Text>Complete</Text>
-          </TouchableOpacity>
-          <Text style={styles.text}>{todo.completed ? 'Completed' : 'Not Completed'}</Text>
+          <CompleteToggle
+            checked={todo.completed}
+            onChecked={() => completeTodo(index)}
+            onUnchecked={() => incompleteTodo(index)}
+            />
           <Text style={styles.text}>{todo.name}</Text>
         </View>
       )
